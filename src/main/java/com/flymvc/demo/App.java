fe1ad3411dabbb7bd624c93be8d159101d12de0a
foreyer <274410087@qq.com>
2017-10-18 12:37:48 +0800

    Change-Id: Ib1873cde71df265c42b2605f9ef364360b8cdddd

--- src/main/java/com/flymvc/demo/App.java
+++ src/main/java/com/flymvc/demo/App.java
@@ -1,26 +1,35 @@
 package com.flymvc.demo;
 
-import com.flymvc.config.Fly;
-import com.flymvc.config.FlyConfig;
+import java.util.List;
+
 import com.flymvc.core.BootStrap;
 import com.flymvc.demo.controller.RoleController;
 import com.flymvc.demo.controller.UserController;
+import com.flymvc.plugin.DbPlugin;
+import com.flymvc.plugin.Plugin;
+import com.flymvc.render.JspRender;
+import com.flymvc.render.Render;
+import com.flymvc.route.RouteMatcher;
 
 public class App extends BootStrap{
 
-	
 	@Override
-	public void config(FlyConfig config) {
+	public void route(RouteMatcher routeMatcher) {
 		// TODO Auto-generated method stub
+		routeMatcher.addRoute("/user", new UserController());
+		routeMatcher.addRoute("/", new UserController(),"test");
+		routeMatcher.addRoute("/role", new RoleController());
 	}
 
 	@Override
-	public void start(Fly fly) {
+	public void plugin(List<Plugin> plugins) {
 		// TODO Auto-generated method stub
-		fly.addRoute("/user", new UserController());
-		fly.addRoute("/", new UserController(),"test");
-		fly.addRoute("/role", new RoleController());
+		plugins.add(new DbPlugin("com.mysql.jdbc.Driver", "root", "root", "jdbc:mysql://127.0.0.1:3306/adminlte-admin?characterEncoding=utf8&zeroDateTimeBehavior=convertToNull"));
 	}
 	
-	
+	@Override
+	public Render render() {
+		// TODO Auto-generated method stub
+		return new JspRender();
+	}
 }
